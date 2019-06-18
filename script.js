4273692beb5b8b2aa7e70d144f433f6e553a95cf
Molchanova Tatyana <mtuspeh@gmail.com>
2019-06-18 21:11:31 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function fun1() {
+    var radi = document.getElementsByName('r1');
+    for (var i = 0; i < radi.length; i++) {
+        if (radi[i].checked) {
+            alert('Checked ' + i + ' element');
+        }
+    }
+}
